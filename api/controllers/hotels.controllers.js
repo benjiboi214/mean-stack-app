84ee4b571beca1f07a860be3f014b3309d3cd067
Ben Elliot <benjiboi214@gmail.com>
2017-09-07 12:11:21 +1000

    Added geospacial query to the hotelsGetAll function

--- api/controllers/hotels.controllers.js
+++ api/controllers/hotels.controllers.js
@@ -1,18 +1,51 @@
 var mongoose = require('mongoose');
 var Hotel = mongoose.model('Hotel');
 
+var runGeoQuery = function(req, res) {
+
+  var lng = parseFloat(req.query.lng);
+  var lat = parseFloat(req.query.lat);
+
+  // A geoJSON point
+  var point = {
+    type : "Point",
+    coordinates : [lng, lat]
+  };
+
+  var geoOptions = {
+    spherical : true,
+    maxDistance: 2000,
+    nun : 5
+  };
+
+  Hotel
+    .geoNear(point, geoOptions, function(err, results, stats) {
+      console.log("Geo results", results);
+      console.log("Geo stats", stats);
+      res
+        .status(200)
+        .json(results);
+    });
+
+};
+
 module.exports.hotelsGetAll = function(req, res) {
 
   var offset = 0;
   var count = 5;
 
+  if (req.query && req.query.lat && req.query.lng) {
+    runGeoQuery(req, res);
+    return;
+  };
+
   if (req.query && req.query.offset) {
     offset = parseInt(req.query.offset, 10);
-  }
+  };
 
   if (req.query && req.query.count) {
     count = parseInt(req.query.count, 10);
-  }
+  };
 
   Hotel
     .find()
@@ -23,9 +56,9 @@ module.exports.hotelsGetAll = function(req, res) {
       res
         .status(200)
         .json(hotels);
-    })
+    });
 
-}
+};
 
 module.exports.hotelsGetOne = function(req, res) {
 
@@ -40,7 +73,7 @@ module.exports.hotelsGetOne = function(req, res) {
         .json(hotel);
     })
 
-}
+};
 
 module.exports.hotelsAddOne = function(req, res) {
 
@@ -67,4 +100,4 @@ module.exports.hotelsAddOne = function(req, res) {
       .status(400)
       .json({ message : "Required data missing from body" });
   };
-}
+};

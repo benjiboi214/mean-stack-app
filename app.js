06f7d75ff2f283d98505b2d0e4ff5b913d712f62
Ben Elliot <benjiboi214@gmail.com>
2017-09-08 16:20:14 +1000

    Added node_modules as a static path, cleanup up comments.

--- app.js
+++ app.js
@@ -6,21 +6,27 @@ var bodyParser = require('body-parser');
 
 var routes = require('./api/routes');
 
+// Define the port to run the application on
 app.set('port', 3000);
 
-app.use('/favicon.ico', function() {return});
-
+// Add logging middleware to log requests
 app.use(function(req, res, next) {
   console.log(req.method, req.url);
   next();
 });
 
+// Set static directories and favicon trap.
+app.use('/favicon.ico', function() {return;});
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/node_modules', express.static(path.join(__dirname, 'node_modules')));
 
+// Enable parsing of posted forms
 app.use(bodyParser.urlencoded({ extended : false }));
 
+// Add some routes
 app.use('/api', routes);
 
+// Listen for requests
 var server = app.listen(app.get('port'), function() {
   var port = server.address().port;
   console.log("Magic happens on port " + port);

ed5a098106579cd1f8d9bc1dec13861efbbf45ec
Ben Elliot <benjiboi214@gmail.com>
2017-09-11 13:43:08 +1000

    Added hotel-rating component to display stars based on rating of hotel.

---
+++ public/angular-app/hotel-rating/hotel-rating-directive.js
@@ -0,0 +1,25 @@
+var hotelRating = {
+  bindings: {
+    stars: "="
+  },
+  template: '<span ng-repeat="star in vm.stars track by $index" class="glyphicon glyphicon-star">{{ star }}</span>',
+  controller: 'HotelController',
+  controllerAs: 'vm',
+};
+
+angular.module('meanhotel')
+  //.directive('hotelRating', hotelRating);
+  .component('hotelRating', hotelRating);
+
+//function hotelRating() {
+//  return {
+//    restrict: 'E',
+//    template: '<span ng-repeat="star in vm.stars track by $index" class="glyphicon glyphicon-star">{{ star }}</span>',
+//    bindToController: true,
+//    controller: 'HotelController',
+//    controllerAs: 'vm',
+//    scope: {
+//      stars: '@'
+//    }
+//  };
+//}

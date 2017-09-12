befd6dc661c9915f6124b99ff5de72282d72e77c
Ben Elliot <benjiboi214@gmail.com>
2017-09-12 11:40:33 +1000

    Added nav bar directive + custome css.

---
+++ public/angular-app/navigation-directive/navigation-directive.js
@@ -0,0 +1,9 @@
+angular.module('meanhotel')
+  .directive('mhNavigation', mhNavigation);
+
+function mhNavigation() {
+  return {
+    restric: 'E',
+    templateUrl: 'angular-app/navigation-directive/navigation-directive.html'
+  };
+}
